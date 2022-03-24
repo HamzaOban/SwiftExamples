ec185674a5d5584f84360e0d8ffa944af7d6d22b
Hamza Oban <hamzaoban3@gmail.com>
2022-03-24 22:11:11 +0300

    Initial Commit

---
+++ Shared/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  Shared
+//
+//  Created by Hamza Oban on 24.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
